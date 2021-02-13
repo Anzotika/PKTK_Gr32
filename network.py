5e4c796b06dbcd6ce9df7cbc7f4bb9f809ea3b25
Ensare <58053060+Ensare@users.noreply.github.com>
2021-02-13 11:10:23 +0100

    Update network.py

--- network.py
+++ network.py
@@ -35,20 +35,7 @@ class TCP:
         self.flag_fin = offset_reserved_flags & 1
         self.data = raw_data[offset:]
         
-class Pcap:
 
-    def __init__(self, filename, link_type=1):
-        self.pcap_file = open(filename, 'wb')
-        self.pcap_file.write(struct.pack('@ I H H i I I I', 0xa1b2c3d4, 2, 4, 0, 0, 65535, link_type))
-
-    def write(self, data):
-        ts_sec, ts_usec = map(int, str(time.time()).split('.'))
-        length = len(data)
-        self.pcap_file.write(struct.pack('@ I I I I', ts_sec, ts_usec, length, length))
-        self.pcap_file.write(data)
-
-    def close(self):
-        self.pcap_file.close()
         
 class IPv4:
 
